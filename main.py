72da11f314f5057766e7158272b54ecd767ae6ba
lukesfather2011 <lukesfather0826@gmail.com>
2024-07-17 21:46:53 -0800

    adjust screen size as well as map size (height and width). Experiment with max/min room size and max_rooms.

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ from input_handlers import EventHandler
 from procgen import generate_dungeon
 
 def main() -> None:
-    screen_width = 80
-    screen_height = 50
+    tile_width = 10
+    tile_height = 10
 
-    map_width = 80
-    map_height = 45
+    screen_width = 1600
+    screen_height = 900
 
-    room_max_size = 10
-    room_min_size = 6
-    max_rooms = 30
+    map_width = screen_width // tile_width
+    map_height = screen_height // tile_height
+
+    room_max_size = 20
+    room_min_size = 10
+    max_rooms = 12
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -23,8 +26,8 @@ def main() -> None:
 
     event_handler = EventHandler()
 
-    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
-    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+    player = Entity(int(map_width / 2), int(map_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(map_width / 2 - 5), int(map_height / 2), "@", (255, 255, 0))
     entities = {npc, player}
 
     game_map = generate_dungeon(
@@ -38,14 +41,14 @@ def main() -> None:
 
     engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
-    with tcod.context.new_terminal(
-            screen_width,
-            screen_height,
+    with tcod.context.new(
+            width=screen_width,
+            height=screen_height,
             tileset=tileset,
             title="Yet Another Roguelike Tutorial",
             vsync=True,
     ) as context:
-        root_console = tcod.console.Console(screen_width, screen_height, order="F")
+        root_console = tcod.console.Console(map_width, map_height, order="F")
         while True:
             engine.render(console=root_console, context=context)
 
